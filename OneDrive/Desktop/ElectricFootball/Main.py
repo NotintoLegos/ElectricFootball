aa10a72e19000d14fa140cd59793fd07e01f1f57
John Hope <jhope4@depaul.edu>
2024-11-12 16:04:52 -0600

    game window

--- OneDrive/Desktop/ElectricFootball/Main.py
+++ OneDrive/Desktop/ElectricFootball/Main.py
@@ -1,3 +1,20 @@
 import pygame
 import time
 import random
+
+WIDTH, HEIGHT= 1000, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Electric Football")
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type== pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
