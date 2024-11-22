59d16b0e5a531f29b3d05434c08910ce7b94290a
John Hope <jhope4@depaul.edu>
2024-11-22 11:06:47 -0600

    changing player to class for OOP

--- OneDrive/Desktop/ElectricFootball/Main.py
+++ OneDrive/Desktop/ElectricFootball/Main.py
@@ -13,22 +13,42 @@ pygame.display.set_caption("Electric Football")
 BG= pygame.transform.scale(pygame.image.load("g558.jpg"), (WIDTH, HEIGHT))
 FONT= pygame.font.SysFont("ariel", 60)
 
-def draw(player, elapsed_time):
+
+class Player:
+    def __init__(self, x, y, width, height, color):
+        self.rect= pygame.Rect(x,y,width, height)
+        self.color= color
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, self.rect)
+    def move(self, keys):
+        if keys[pygame.K_LEFT] and player.x- PLAYER_VEL >= 0:
+            player.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL+ player.width <= WIDTH:
+            player.x += PLAYER_VEL
+        if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
+            player.y += PLAYER_VEL
+        if keys[pygame.K_UP] and player.y- PLAYER_VEL >= 0:
+            player.y -= PLAYER_VEL
+
+def draw(win, players, elapsed_time):
     WIN.blit(BG, (0, 0))
 
     time_text= FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
 
-    pygame.draw.rect(WIN, "blue", player)
+    for player in players:
+        player.draw(win)
+
 
     pygame.display.update()
+    
 
 def main():
     run = True
-
-    player= pygame.Rect(200, HEIGHT-BALL_H, BALL_W, BALL_H)
-
     clock= pygame.time.Clock()
+
+    player= Player(200, HEIGHT-BALL_H, BALL_W, BALL_H, "blue")
+    ball_carrier= Player(300, HEIGHT-BALL_H, BALL_W, BALL_H, "brown")
 
     start_time= time.time()
     elapsed_time= 0
@@ -44,18 +64,7 @@ def main():
                 
         keys= pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT] and player.x- PLAYER_VEL >= 0:
-            player.x -= PLAYER_VEL
-        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL+ player.width <= WIDTH:
-            player.x += PLAYER_VEL
-        if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
-            player.y += PLAYER_VEL
-        if keys[pygame.K_UP] and player.y- PLAYER_VEL >= 0:
-            player.y -= PLAYER_VEL
-
-        draw(player, elapsed_time)
-
-
+        draw(WIN, [player, ball_carrier], elapsed_time)
 
     pygame.quit()
 
