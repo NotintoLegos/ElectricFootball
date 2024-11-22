10e88007b6a4082aec08e9eb4bb84c06a5550422
John Hope <jhope4@depaul.edu>
2024-11-22 14:25:57 -0600

    added random movement to ball_carrier

--- OneDrive/Desktop/ElectricFootball/Main.py
+++ OneDrive/Desktop/ElectricFootball/Main.py
@@ -6,7 +6,7 @@ pygame.font.init()
 
 BALL_H, BALL_W= 10, 10
 WIDTH, HEIGHT= 1200, 550
-PLAYER_VEL= 5
+PLAYER_VEL= 2
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Electric Football")
@@ -18,26 +18,39 @@ class Player:
     def __init__(self, x, y, width, height, color):
         self.rect= pygame.Rect(x,y,width, height)
         self.color= color
+
     def draw(self, window):
         pygame.draw.rect(window, self.color, self.rect)
-    def move(self, keys):
-        if keys[pygame.K_LEFT] and player.x- PLAYER_VEL >= 0:
-            player.x -= PLAYER_VEL
-        if keys[pygame.K_RIGHT] and player.x + PLAYER_VEL+ player.width <= WIDTH:
-            player.x += PLAYER_VEL
-        if keys[pygame.K_DOWN] and player.y + PLAYER_VEL + player.height <= HEIGHT:
-            player.y += PLAYER_VEL
-        if keys[pygame.K_UP] and player.y- PLAYER_VEL >= 0:
-            player.y -= PLAYER_VEL
 
-def draw(win, players, elapsed_time):
+    def move(self, keys):
+        if keys[pygame.K_LEFT] and self.rect.x- PLAYER_VEL >= 0:
+            self.rect.x -= PLAYER_VEL
+        if keys[pygame.K_RIGHT] and self.rect.x + PLAYER_VEL+ self.rect.width <= WIDTH:
+            self.rect.x += PLAYER_VEL
+        if keys[pygame.K_DOWN] and self.rect.y + PLAYER_VEL + self.rect.height <= HEIGHT:
+            self.rect.y += PLAYER_VEL
+        if keys[pygame.K_UP] and self.rect.y- PLAYER_VEL >= 0:
+            self.rect.y -= PLAYER_VEL
+    
+    def tackle(self, other):
+        return self.rect.collidedict(other.rect)
+
+    def move_randomly(self):
+        directions= [(PLAYER_VEL, 0), (-PLAYER_VEL, 0), (0,PLAYER_VEL), (0, -PLAYER_VEL)]
+        dx, dy = random.choice(directions)
+        if 0 <= self.rect.x + dx <= WIDTH - self.rect.width:
+            self.rect.x += dx
+        if 0 <= self.rect.y + dy <= HEIGHT - self.rect.height:
+            self.rect.y += dy
+
+def draw(WIN, players, elapsed_time):
     WIN.blit(BG, (0, 0))
 
     time_text= FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
 
     for player in players:
-        player.draw(win)
+        player.draw(WIN)
 
 
     pygame.display.update()
@@ -47,7 +60,7 @@ def main():
     run = True
     clock= pygame.time.Clock()
 
-    player= Player(200, HEIGHT-BALL_H, BALL_W, BALL_H, "blue")
+    player1= Player(200, HEIGHT-BALL_H, BALL_W, BALL_H, "blue")
     ball_carrier= Player(300, HEIGHT-BALL_H, BALL_W, BALL_H, "brown")
 
     start_time= time.time()
@@ -63,8 +76,10 @@ def main():
                 break
                 
         keys= pygame.key.get_pressed()
+        player1.move(keys)
 
-        draw(WIN, [player, ball_carrier], elapsed_time)
+        draw(WIN, [player1, ball_carrier], elapsed_time)
+        ball_carrier.move_randomly()
 
     pygame.quit()
 
