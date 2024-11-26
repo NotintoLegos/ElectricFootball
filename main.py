45b50a5c5c2ad621a65ccf2ce52deba532c1e4cc
John Hope <john.hope1997@gmail.com>
2024-11-26 11:46:29 -0600

    changing line setup, added d player

--- main.py
+++ main.py
@@ -37,8 +37,24 @@ def main():
     run = True
     clock= pygame.time.Clock()
 
-    player1= Player(200, HEIGHT-BALL_H, BALL_W, BALL_H, "brown")
+#testing at random location 550
+# x value when tackled to set scrimmage placement
+    SCRIMMAGE_PLACEMENT= 550
+    first_down_distance= SCRIMMAGE_PLACEMENT-100
+#to set players off the line slightly since line width is 3
+    defense_on_line_setup= SCRIMMAGE_PLACEMENT - 12
+    offense_on_line_setup= SCRIMMAGE_PLACEMENT + 5
+
+# y value when tackle to decide start or ball placement, 300 set for testing
+    y_value= 300
+
+# object creation, don't know if I should make a factory since its max of 22 players
+    player1= Player(offense_on_line_setup, y_value, BALL_W, BALL_H, "brown")
+    player2= Player(defense_on_line_setup, y_value, BALL_W, BALL_H, "teal")
     line_scrimmage= Lines(SCRIMMAGE_PLACEMENT, 0, SCRIMMAGE_WIDTH, SCRIMMAGE_HEIGHT, "blue")
+    first_down_line= Lines(first_down_distance, 0, 3, 550, "yellow")
+    goal_line_left= Lines(97, 0, 3, 550, "white")
+    goal_line_right= Lines(1100, 0, 3, 550, "white")
 
     start_time= time.time()
     elapsed_time= 0
@@ -55,7 +71,7 @@ def main():
         keys= pygame.key.get_pressed()
         player1.move(keys, PLAYER_VEL, WIDTH, HEIGHT)
 
-        draw(WIN, [player1], [line_scrimmage], elapsed_time)
+        draw(WIN, [player1, player2], [line_scrimmage, first_down_line, goal_line_left, goal_line_right], elapsed_time)
 
     pygame.quit()
 
